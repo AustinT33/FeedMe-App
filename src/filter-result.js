31f863dd1d1f6ed91d7ad878d359a47b732256f1
AustinT33 <austintumlinson@gmail.com>
2019-12-30 11:53:20 -0600

    working on filter

--- src/filter-result.js
+++ src/filter-result.js
@@ -4,8 +4,9 @@ import places from './store'
 function Filtered() {
     const filteredResult = places.filter(place => place.price ==='$')
     console.log(filteredResult)
+    const filteredChoice = filteredResult[0].name
         return (
-            <div>choices={filteredResult}</div>
+            <div>choices={filteredChoice}</div>
 
         )
     }
