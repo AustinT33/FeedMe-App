06bbb30fbf9336078388e95677f57f7bc612f7c1
AustinT33 <austintumlinson@gmail.com>
2020-01-10 13:14:56 -0600

    fixed endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    // API_ENDPOINT: 'https://rocky-spire-28413.herokuapp.com',
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://rocky-spire-28413.herokuapp.com',
     TOKEN_KEY: 'feedme-client-auth-token',
 }
